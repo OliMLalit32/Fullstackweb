5a61e3fdba07c66cc8bbfb4812638ce2d93ded6c
Lalit Mohan Oli <lalitmohanoli4532@gmail.com>
2026-05-26 06:42:57 +0530

    Update

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,14 @@
 mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: "mapbox://styles/mapbox/streets-v12", // A default style
-    center: listing.geometry.coordinates, // Use listing's coordinates to center the map
-    zoom: 9 // starting zoom
+    container: 'map',
+    style: "mapbox://styles/mapbox/streets-v12", 
+    center: listing.geometry.coordinates, 
+    zoom: 9 
 });
 
-// Create a marker with a popup
 const marker = new mapboxgl.Marker({ color: "red" })
-    .setLngLat(listing.geometry.coordinates) // Set marker at listing's coordinates
+    .setLngLat(listing.geometry.coordinates) 
     .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(
         `<h6>${listing.title}</h6><p>Exact location provided after booking</p>`
     ))
