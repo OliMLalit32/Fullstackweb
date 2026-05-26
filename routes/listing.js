ef205f61f9b31a1a45137140a70bf85b3588e176
Lalit Mohan Oli <lalitmohanoli4532@gmail.com>
2026-05-26 06:44:38 +0530

    Update

--- routes/listing.js
+++ routes/listing.js
@@ -9,24 +9,17 @@ const multer = require('multer');
 const {storage} = require("../cloudConfig.js");
 const upload = multer({storage});
 
-
-
-// Index and Create ("/")
 router.route("/")
-    .get(wrapAsync(listingController.index)) // index route
-    .post(isLoggedIn,  upload.single('listing[image]'),validateListing, wrapAsync(listingController.createListing)); // create route
+    .get(wrapAsync(listingController.index)) 
+    .post(isLoggedIn,  upload.single('listing[image]'),validateListing, wrapAsync(listingController.createListing)); 
     
-
-// New route (separate path) -> should be before show and update and delete(why?)
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-// Show, Update, and Delete ("/:id")
 router.route("/:id")
-    .get(wrapAsync(listingController.showListing)) // show route
-    .put(isLoggedIn, isOwner,upload.single('listing[image]'), validateListing, wrapAsync(listingController.updateListing)) // update route
-    .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing)); // delete route
+    .get(wrapAsync(listingController.showListing)) 
+    .put(isLoggedIn, isOwner,upload.single('listing[image]'), validateListing, wrapAsync(listingController.updateListing)) 
+    .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing)); 
 
-// Edit route (separate path)
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
 
